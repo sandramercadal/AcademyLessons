86eeae755926126f9e20286e63582963411f8fec
Sandra Mercadal <sandra.mercadal@Mac.lan>
2025-03-31 15:10:24 +0100

    Updated worksheets

--- src/main/scala/Week2/Monday/Library/LibraryLogic.scala
+++ src/main/scala/Week2/Monday/Library/LibraryLogic.scala
@@ -9,14 +9,18 @@ import Week2.Monday.family.{Adult, Forename, Surname}
 
   object LibraryLogic extends App {
 
+    //CREATE SOME BOOKS
     val harryPotter: Book = new Book("Harry Potter", Adult(new Forename("J.K"), new Surname("Rowling"))) with Fantasy with AudioBook with DigitalBook with PhysicalBook
-
-    println(harryPotter.author.forename.name)
-    println(harryPotter.title)
-    println(harryPotter.read)
-    println(harryPotter.enjoy)
+//it has characteristics of a fantasy book and can be in audio, digital, and physical formats.
 
     val goneGirl: Book = new Book("Gone Girl",Adult(new Forename("Gillian"), new Surname("Flynn"))) with Thriller
-    println(goneGirl.enjoy)
+
+    println(harryPotter.author.forename.name) //J.K
+    println(harryPotter.title) //Harry Potter
+    println(harryPotter.read) //Enjoy reading!
+    println(harryPotter.enjoy) //Enjoy your fantasy book!
+    println(goneGirl.enjoy) //Enjoy your thriller book!
+
+
 
   }
