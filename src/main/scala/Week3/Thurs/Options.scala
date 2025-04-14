dc30dd521b64350c86a30a0b5539f114e9dd84fa
Sandra Mercadal <sandra.mercadal@Mac.lan>
2025-04-14 08:04:53 +0100

    Updated Worksheets

--- src/main/scala/Week3/Thurs/Options.scala
+++ src/main/scala/Week3/Thurs/Options.scala
@@ -7,11 +7,10 @@
 //Lets think about our pizza restaurant again.
 //If the customer doesn’t specify their preferred crust, the restaurant will just give a classic crust by default.
 //This is how we would change our case class:
-//How to change a case clas to include and Option // case class Pizza (size: Int, crust: Option[String])
+//How to change a case class to include and Option // case class Pizza (size: Int, crust: Option[String])
 
 //This code uses a trait to define a common structure for different types of pizzas,
-// while utilizing Scala's Option type to handle cases where a crust might or might
-// not be specified.
+// while utilizing Scala's Option type to handle cases where a crust might or might not be specified.
 
 package Week3.Thurs
 
@@ -19,47 +18,53 @@ object Options extends App {
 
   //make a trait so it is consumed in this file. Defines 2 properties
   //if they don't specify the crust they get the default, I wrap it in the option of a string
-
+//the crust can either have a value (like "classic" or "stuffed") or be None, meaning there's no crust selected.
   trait Pizza {
     val size: Int
-    val crust: Option[String]
+    val crust: Option[String] // crust is OPTION (an optional string).
   }
-//4 diff pizza objects that extend the pizza trait
-  object Personal extends Pizza {
+
+  /**You create four different pizzas objects by extending the Pizza trait:**/
+  //Notice override val - when you define a trait or a class, the properties you declare are essentially abstract unless you provide a concrete implementation.
+//Personal: 7 inches with a classic crust.
+//Small: 9 inches with no crust (None). As no crust chosen, will default to classic
+//Medium: 11 inches with a classic crust.
+//Large: 14 inches with no crust (None). As no crust chosen, will default to classic
+
+  object Personalised extends Pizza {
     override val size: Int = 7
-    override val crust: Option[String] = Some("classic") //Must wrap in a Some if we are giving it 'SOMEthing'
+    override val crust: Option[String] = Some("classic") //Must wrap in a Some if we are giving it 'something'
   }
 
   object Small extends Pizza {
     override val size: Int = 9
-    override val crust: Option[String] = None //Not giving a default
+    override val crust: Option[String] = None //With no crust choice so classic by default
   }
 
   object Medium extends Pizza {
     override val size: Int = 11
-    override val crust: Option[String] = Some("classic") //assigning a default
+    override val crust: Option[String] = Some("classic") //assigning a default classic crust
   }
 
   object Large extends Pizza {
     override val size: Int = 14
-    override val crust: Option[String] = None
+    override val crust: Option[String] = None //With no crust
   }
 
-
-  //val for a new pizzaOrder
-  val newPizzaOrder: Pizza = new Pizza {
+  /**Create some instances of Pizza orders **/
+  //14" with stuffed crust
+  val newPersonalisedOrder: Pizza = new Pizza {
     override val size: Int = 14
     override val crust: Option[String] = Some("stuffed") //Option is wrapped in a some
   }
 
-
-  val emptyCrustPizzaOrder: Pizza = new Pizza {
+  //14" with no special crust so default
+  val noCrustSpecifiedOrder: Pizza = new Pizza {
     override val size: Int = 14
     override val crust: Option[String] = None
   }
 
-
-  //method for crust only
+  /**method to determine crust type **/
   def whatCrust(pizza: Pizza): String = {
     //pizza has a size and a crust
     //tab onto match exhaustive
@@ -70,6 +75,38 @@ object Options extends App {
       case None => "just the default classic crust"
     }
   }
+
+  /**Testing all the Pizzas**/
+  println("Pre-defined pizzas:")
+  println(s"Personalised: Size = ${Personalised.size}, Crust = ${whatCrust(Personalised)}")
+  println(s"Small: Size = ${Small.size}, Crust = ${whatCrust(Small)}")
+  println(s"Medium: Size = ${Medium.size}, Crust = ${whatCrust(Medium)}")
+  println(s"Large: Size = ${Large.size}, Crust = ${whatCrust(Large)}")
+
+  println("\nCustom orders:")
+  println(s"New Personalised Order: Size = ${newPersonalisedOrder.size}, Crust = ${whatCrust(newPersonalisedOrder)}")
+  println(s"No Crust Specified Order: Size = ${noCrustSpecifiedOrder.size}, Crust = ${whatCrust(noCrustSpecifiedOrder)}")
+
+  // Demonstrating getOrElse
+  println("\nDemonstrating getOrElse:")
+  println(s"newPersonalisedOrder crust: ${newPersonalisedOrder.crust.getOrElse("classic")}")
+  println(s"noCrustSpecifiedOrder crust: ${noCrustSpecifiedOrder.crust.getOrElse("classic")}")
+}
+
+
+
+
+
+
+  //println(newPersonalisedOrder) //stuffed crust
+  //println(emptyCrustPizzaOrder) //
+
+
+  //println(s"New Pizza Order: Size = ${newPizzaOrder.size}, Crust = ${newPizzaOrder.crust.getOrElse("No crust")}")
+
+  //println(s"Empty Crust Pizza Order: Size = ${emptyCrustPizzaOrder.size}, Crust = ${emptyCrustPizzaOrder.crust.getOrElse("No crust")}")
+
+
   //Some & None defaults to a none option Pizza (14, None)
   //  //Some Pizza (14, Some,
   //  //both sides need to be warpped in the option
@@ -77,7 +114,9 @@ object Options extends App {
 
   // you have to call a method for it to run
   //we have to println we are in an object
-  println(whatCrust(newPizzaOrder)) //returns
+  //println(whatCrust(newPersonalisedOrder)) //stuffed crust
+
+
   //test the opposite override val crust: Option[String] = None ("stuffed") ??
 
 
@@ -88,7 +127,9 @@ object Options extends App {
   //This will not work with the none. you get error none.get so need to provide it with the catch.
   //println(emptyCrustPizzaOrder.crust.getOrElse ("classic"))
   //get what is in the sum or give me this
-}
+
+
+
 
 
 
